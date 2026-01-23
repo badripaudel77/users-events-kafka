77e64d0a6dbb775bbd3b21ac20c02d9877388333
Badri Paudel <thecrazymanav@gmail.com>
2026-01-22 21:03:02 -0600

    Retry mechanism added

--- src/main/java/edu/miu/ea/kafkaevents/consumer/UserEventConsumer.java
+++ src/main/java/edu/miu/ea/kafkaevents/consumer/UserEventConsumer.java
@@ -1,7 +1,9 @@
 package edu.miu.ea.kafkaevents.consumer;
 
 import edu.miu.ea.kafkaevents.model.UserRegisteredEvent;
+import org.springframework.kafka.annotation.BackOff;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.RetryableTopic;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -9,6 +11,12 @@ public class UserEventConsumer {
 
     // Sending the email to the user for confirmation
     // Event comes as string, but since we have used deserializer, it would be deserialized.
+    // Try three times on an interval of 2s when it throws exception
+    @RetryableTopic(
+            attempts = "3",
+            backOff = @BackOff(delay = 2000),
+            autoCreateTopics = "true"
+    )
     @KafkaListener(topics = "user.emailevents", groupId = "user-event-group")
     public void consume(UserRegisteredEvent event) {
         System.out.println("Consumed : Confirmation Email sent, please verify -> " + event);
